c4aeaeff9ee05d20a9267a437d58c8c380b0ea3c
Mateusz Filipowicz <mateusz.filipowicz@roche.com>
2025-02-23 23:27:18 +0100

    feat: initialize Terraform Plugin Framework

---
+++ internal/utils/env.go
@@ -0,0 +1,50 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package utils
+
+import (
+	"os"
+	"strconv"
+)
+
+// GetAnyStringEnv returns the first non-empty string value from the environment variables.
+func GetAnyStringEnv(ks ...string) string {
+	for _, k := range ks {
+		if v := os.Getenv(k); v != "" {
+			return v
+		}
+	}
+
+	return ""
+}
+
+// GetAnyBoolEnv returns the first non-empty boolean value from the environment variables.
+func GetAnyBoolEnv(ks ...string) bool {
+	val := ""
+
+	for _, k := range ks {
+		if v := os.Getenv(k); v != "" {
+			val = v
+			break
+		}
+	}
+
+	return val == "true" || val == "1"
+}
+
+// GetAnyIntEnv returns the first non-empty integer value from the environment variables.
+func GetAnyIntEnv(ks ...string) int {
+	for _, k := range ks {
+		if v := os.Getenv(k); v != "" {
+			if i, err := strconv.Atoi(v); err == nil {
+				return i
+			}
+		}
+	}
+
+	return 0
+}
