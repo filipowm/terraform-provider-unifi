0d103a38c0a6cd3ea8c617049bbe5392921b87df
Mateusz Filipowicz <mateusz.filipowicz@roche.com>
2025-03-14 01:05:56 +0100

    feat: add support for UniFi Switch (USW) settings with `unifi_setting_usw` resource

---
+++ internal/provider/acctest/resource_setting_usw_test.go
@@ -0,0 +1,47 @@
+package acctest
+
+import (
+	"fmt"
+	pt "github.com/filipowm/terraform-provider-unifi/internal/provider/testing"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+	"sync"
+	"testing"
+)
+
+var settingUswLock = &sync.Mutex{}
+
+func TestAccSettingUsw(t *testing.T) {
+	AcceptanceTest(t, AcceptanceTestCase{
+		Lock: settingUswLock,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSettingUswConfig(true),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("unifi_setting_usw.test", "id"),
+					resource.TestCheckResourceAttr("unifi_setting_usw.test", "site", "default"),
+					resource.TestCheckResourceAttr("unifi_setting_usw.test", "dhcp_snoop", "true"),
+				),
+				ConfigPlanChecks: pt.CheckResourceActions("unifi_setting_usw.test", plancheck.ResourceActionCreate),
+			},
+			pt.ImportStepWithSite("unifi_setting_usw.test"),
+			{
+				Config: testAccSettingUswConfig(false),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("unifi_setting_usw.test", "id"),
+					resource.TestCheckResourceAttr("unifi_setting_usw.test", "site", "default"),
+					resource.TestCheckResourceAttr("unifi_setting_usw.test", "dhcp_snoop", "false"),
+				),
+				ConfigPlanChecks: pt.CheckResourceActions("unifi_setting_usw.test", plancheck.ResourceActionUpdate),
+			},
+		},
+	})
+}
+
+func testAccSettingUswConfig(dhcpSnoop bool) string {
+	return fmt.Sprintf(`
+resource "unifi_setting_usw" "test" {
+	dhcp_snoop = %t
+}
+`, dhcpSnoop)
+}
